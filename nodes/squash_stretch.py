b0f08d4db2d2e39df594bc58ff6df6831eae2095
Billy Okal <okal.billy@gmail.com>
2013-07-18 19:14:49 +0200

    reorganized code, setting the basis animation principles

---
+++ nodes/squash_stretch.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python
+from dmp_interface import *
+import pylab as  plt
+
+
+
+def create_demo_traj_squash(size, dim=1):
+    if dim == 1:
+        x = np.linspace(0, np.pi/2.0, size)
+        y = abs(np.cos(x))
+    else:
+        x = np.linspace(np.pi/2.0, np.pi, size)
+        y = abs(np.cos(x))
+
+    return y.tolist()
+
+def create_demo_arc(size):
+    x = np.linspace(0, 1, size)
+    return (np.exp(x**2)/np.sqrt(2*np.pi)).tolist()
+
+
+
+def squash_and_stretch():
+    y1 = create_demo_traj_squash(500, 1)
+    y2 = create_demo_traj_squash(500, 2)
+    # y2 = create_demo_arc(500)
+
+    dims = 2
+    dt = 1.0                
+    K = 50                 
+    D = 2.0 * np.sqrt(K)      
+    num_bases = 50
+
+    assert(len(y1) == len(y2))
+    traj = [[y1[i],y2[i]] for i in xrange(len(y1))]
+    resp = makeLFDRequest(dims, traj, dt, K, D, num_bases)
+    makeSetActiveRequest(resp.dmp_list)
+
+
+    print 'generating plan'
+    #Now, generate a plan
+    x_0 = [y1[0], y2[0]]          #Plan starting at a different point than demo 
+    x_dot_0 = [y1[0], y2[0]]   
+    # x_dot_0 = [y1[0], y1[-1]]   
+    t_0 = 0                
+    goal = [y1[-1], y2[-1]]         #Plan to a different goal than demo
+    goal_thresh = [0.2, 0.2]
+    seg_length = -1          #Plan until convergence to goal
+    tau = 1.2 * resp.tau       #Desired plan should take twice as long as demo
+    dt = 1.0
+    integrate_iter = 5       #dt is rather large, so this is > 1  
+    plan = makePlanRequest(x_0, x_dot_0, t_0, goal, goal_thresh, 
+                           seg_length, tau, dt, integrate_iter)
+
+
+    # aggregate reproduced trajectories
+    rp1, rp2 = [], []
+    rv1, rv2 = [], []
+    # print plan
+
+    reproduced_traj = plan.plan
+    for p in reproduced_traj.points:
+        rp1.append(p.positions[0])
+        rv1.append(p.velocities[0])
+        rp2.append(p.positions[1])
+        rv2.append(p.velocities[1])
+
+    plt.figure('squash_stretch dmp learning')
+    plt.plot(y1+y2, label='Demonstrated Policy')
+    plt.plot(rp1+rp2, label='Reproduced Policy (1.2*tau)')
+    plt.legend(loc='lower right')
+
+
+    plt.figure('velocities')
+    plt.plot(rv1+rv2, label='Velocities')
+    plt.show()
+
+
+
+def arc():
+    y = create_demo_arc(500)
+
+    dims = 1
+    dt = 1.0                
+    K = 50                 
+    D = 2.0 * np.sqrt(K)      
+    num_bases = 50
+
+    traj = [[y[i]] for i in xrange(len(y))]
+    resp = makeLFDRequest(dims, traj, dt, K, D, num_bases)
+    makeSetActiveRequest(resp.dmp_list)
+
+
+    print 'generating plan'
+    #Now, generate a plan
+    x_0 = [y[0]]          #Plan starting at a different point than demo 
+    x_dot_0 = [y[0]]   
+    t_0 = 0                
+    goal = [y[-1]]         #Plan to a different goal than demo
+    goal_thresh = [0.2]
+    seg_length = -1          #Plan until convergence to goal
+    tau = 0.8 * resp.tau       #Desired plan should take twice as long as demo
+    dt = 1.0
+    integrate_iter = 5       #dt is rather large, so this is > 1  
+    plan = makePlanRequest(x_0, x_dot_0, t_0, goal, goal_thresh, 
+                           seg_length, tau, dt, integrate_iter)
+
+
+    # aggregate reproduced trajectories
+    rp = []
+    rv = []
+    # print plan
+
+    reproduced_traj = plan.plan
+    for p in reproduced_traj.points:
+        rp.append(p.positions[0])
+        rv.append(p.velocities[0])
+
+    plt.figure('Arc dmp learning')
+    plt.plot(y, label='Demonstrated Policy')
+    plt.plot(rp, label='Reproduced Policy (0.8*tau)')
+    plt.legend(loc='lower right')
+
+    plt.figure('Velocities')
+    plt.plot(rv, label='Velocities')
+    plt.show()
+
+
+if __name__ == '__main__':
+    rospy.init_node('squash_stretch')
+
+    squash_and_stretch()
+    arc()
+
+    
+
---
+++ src/dmp_interface.py
@@ -0,0 +1,56 @@
+import roslib; 
+roslib.load_manifest('dmp')
+import rospy 
+import numpy as np
+from dmp.srv import *
+from dmp.msg import *
+
+#Learn a DMP from demonstration data
+def makeLFDRequest(dims, traj, dt, K_gain, 
+                   D_gain, num_bases):
+    demotraj = DMPTraj()
+        
+    for i in range(len(traj)):
+        pt = DMPPoint();
+        pt.positions = traj[i]
+        demotraj.points.append(pt)
+        demotraj.times.append(dt*i)
+            
+    k_gains = [K_gain]*dims
+    d_gains = [D_gain]*dims
+        
+    print "Starting LfD..."
+    rospy.wait_for_service('learn_dmp_from_demo')
+    try:
+        lfd = rospy.ServiceProxy('learn_dmp_from_demo', LearnDMPFromDemo)
+        resp = lfd(demotraj, k_gains, d_gains, num_bases)
+    except rospy.ServiceException, e:
+        print "Service call failed: %s"%e
+    print "LfD done"    
+            
+    return resp;
+
+
+#Set a DMP as active for planning
+def makeSetActiveRequest(dmp_list):
+    try:
+        sad = rospy.ServiceProxy('set_active_dmp', SetActiveDMP)
+        sad(dmp_list)
+    except rospy.ServiceException, e:
+        print "Service call failed: %s"%e
+
+
+#Generate a plan from a DMP
+def makePlanRequest(x_0, x_dot_0, t_0, goal, goal_thresh, 
+                    seg_length, tau, dt, integrate_iter):
+    print "Starting DMP planning..."
+    rospy.wait_for_service('get_dmp_plan')
+    try:
+        gdp = rospy.ServiceProxy('get_dmp_plan', GetDMPPlan)
+        resp = gdp(x_0, x_dot_0, t_0, goal, goal_thresh, 
+                   seg_length, tau, dt, integrate_iter)
+    except rospy.ServiceException, e:
+        print "Service call failed: %s"%e
+    print "DMP planning done"   
+            
+    return resp;
