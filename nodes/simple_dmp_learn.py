51979963b6ef8f73e514fd36bc66d3a4bc640f1e
Billy Okal <okal.billy@gmail.com>
2013-07-18 19:23:13 +0200

    minor edits in learning

--- nodes/simple_dmp_learn.py
+++ nodes/simple_dmp_learn.py
@@ -1,60 +1,5 @@
 #!/usr/bin/env python
-import roslib; 
-roslib.load_manifest('dmp')
-import rospy 
-import numpy as np
-from dmp.srv import *
-from dmp.msg import *
-
-#Learn a DMP from demonstration data
-def makeLFDRequest(dims, traj, dt, K_gain, 
-                   D_gain, num_bases):
-    demotraj = DMPTraj()
-        
-    for i in range(len(traj)):
-        pt = DMPPoint();
-        pt.positions = traj[i]
-        demotraj.points.append(pt)
-        demotraj.times.append(dt*i)
-            
-    k_gains = [K_gain]*dims
-    d_gains = [D_gain]*dims
-        
-    print "Starting LfD..."
-    rospy.wait_for_service('learn_dmp_from_demo')
-    try:
-        lfd = rospy.ServiceProxy('learn_dmp_from_demo', LearnDMPFromDemo)
-        resp = lfd(demotraj, k_gains, d_gains, num_bases)
-    except rospy.ServiceException, e:
-        print "Service call failed: %s"%e
-    print "LfD done"    
-            
-    return resp;
-
-
-#Set a DMP as active for planning
-def makeSetActiveRequest(dmp_list):
-    try:
-        sad = rospy.ServiceProxy('set_active_dmp', SetActiveDMP)
-        sad(dmp_list)
-    except rospy.ServiceException, e:
-        print "Service call failed: %s"%e
-
-
-#Generate a plan from a DMP
-def makePlanRequest(x_0, x_dot_0, t_0, goal, goal_thresh, 
-                    seg_length, tau, dt, integrate_iter):
-    print "Starting DMP planning..."
-    rospy.wait_for_service('get_dmp_plan')
-    try:
-        gdp = rospy.ServiceProxy('get_dmp_plan', GetDMPPlan)
-        resp = gdp(x_0, x_dot_0, t_0, goal, goal_thresh, 
-                   seg_length, tau, dt, integrate_iter)
-    except rospy.ServiceException, e:
-        print "Service call failed: %s"%e
-    print "DMP planning done"   
-            
-    return resp;
+from dmp_interface import *
 
 
 def create_lab_demo(size):
@@ -81,13 +26,14 @@ if __name__ == '__main__':
 
     print 'generating plan'
     #Now, generate a plan
-    x_0 = [0.0]          #Plan starting at a different point than demo 
-    x_dot_0 = [0.0]   
+    x_0 = [y[0]]          #Plan starting at a different point than demo 
+    x_dot_0 = [y[0]]   
     t_0 = 0                
-    goal = [y[-1]]         #Plan to a different goal than demo
+    # goal = [y[-1]]         #Plan to a different goal than demo
+    goal = [0.5]
     goal_thresh = [0.2]
     seg_length = -1          #Plan until convergence to goal
-    tau = 1.5 * resp.tau       #Desired plan should take twice as long as demo
+    tau = 1.2 * resp.tau       #Desired plan should take twice as long as demo
     dt = 1.0
     integrate_iter = 5       #dt is rather large, so this is > 1  
     plan = makePlanRequest(x_0, x_dot_0, t_0, goal, goal_thresh, 
@@ -104,8 +50,9 @@ if __name__ == '__main__':
         rp.append(p.positions[0])
         rv.append(p.velocities[0])
 
-    plt.plot(y)
-    plt.plot(rp)
+    plt.plot(y, label='Demonstrated Policy')
+    plt.plot(rp, label='Reproduced Policy (1.2*tau)')
+    plt.legend(loc='lower right')
     plt.show()
 
     # print plan
