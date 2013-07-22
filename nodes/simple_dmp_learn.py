30ed0d3a4a4d03eb20276bf63bf8be32b0a9c998
Billy Okal <okal.billy@gmail.com>
2013-07-22 15:22:47 +0200

    minor edits and fixes

--- nodes/simple_dmp_learn.py
+++ nodes/simple_dmp_learn.py
@@ -5,40 +5,38 @@ from dmp_interface import *
 def create_lab_demo(size):
     x = np.linspace(0, 4, size)
     ts = 0.2
-    y = np.sin(2*(x-ts)) + np.exp(-16*(x-ts)**2)
+    y = np.sin(2 * (x - ts)) + np.exp(-16 * (x - ts) ** 2)
     return y.tolist()
 
 if __name__ == '__main__':
     rospy.init_node('simple_dmp_learn')
 
-    import pylab as  plt
+    import pylab as plt
 
     y = create_lab_demo(500)
     dims = 1
-    dt = 1.0                
-    K = 50                 
-    D = 2.0 * np.sqrt(K)      
+    dt = 1.0
+    K = 50
+    D = 2.0 * np.sqrt(K)
     num_bases = 50
     traj = [[y[i]] for i in xrange(len(y))]
     resp = makeLFDRequest(dims, traj, dt, K, D, num_bases)
     makeSetActiveRequest(resp.dmp_list)
 
-
     print 'generating plan'
-    #Now, generate a plan
-    x_0 = [y[0]]          #Plan starting at a different point than demo 
-    x_dot_0 = [y[0]]   
-    t_0 = 0                
+    # Now, generate a plan
+    x_0 = [y[0]]  # Plan starting at a different point than demo
+    x_dot_0 = [y[0]]
+    t_0 = 0
     # goal = [y[-1]]         #Plan to a different goal than demo
     goal = [0.5]
     goal_thresh = [0.2]
-    seg_length = -1          #Plan until convergence to goal
-    tau = 1.2 * resp.tau       #Desired plan should take twice as long as demo
+    seg_length = -1  # Plan until convergence to goal
+    tau = 1.2 * resp.tau  # Desired plan should take twice as long as demo
     dt = 1.0
-    integrate_iter = 5       #dt is rather large, so this is > 1  
-    plan = makePlanRequest(x_0, x_dot_0, t_0, goal, goal_thresh, 
+    integrate_iter = 5  # dt is rather large, so this is > 1
+    plan = makePlanRequest(x_0, x_dot_0, t_0, goal, goal_thresh,
                            seg_length, tau, dt, integrate_iter)
-
 
     # aggregate reproduced trajectories
     rp = []
@@ -59,29 +57,29 @@ if __name__ == '__main__':
 
 
 
-    #Create a DMP from a 3-D trajectory
-    # dims = 3                
-    # dt = 1.0                
-    # K = 50                 
-    # D = 2.0 * np.sqrt(K)      
-    # num_bases = 4          
+    # Create a DMP from a 3-D trajectory
+    # dims = 3
+    # dt = 1.0
+    # K = 50
+    # D = 2.0 * np.sqrt(K)
+    # num_bases = 4
     # traj = [[1.0,1.0,1.0],[2.0,2.0,1.8],[3.0,4.0,4.5],[6.0,8.0,7.5]]
     # resp = makeLFDRequest(dims, traj, dt, K, D, num_bases)
 
-    # #Set it as the active DMP
+    # Set it as the active DMP
     # makeSetActiveRequest(resp.dmp_list)
 
-    # #Now, generate a plan
-    # x_0 = [0.0,0.0,0.0]          #Plan starting at a different point than demo 
-    # x_dot_0 = [0.0,0.0,0.0]   
-    # t_0 = 0                
+    # Now, generate a plan
+    # x_0 = [0.0,0.0,0.0]          #Plan starting at a different point than demo
+    # x_dot_0 = [0.0,0.0,0.0]
+    # t_0 = 0
     # goal = [8.0,7.0,6.0]         #Plan to a different goal than demo
     # goal_thresh = [0.2,0.2,0.2]
     # seg_length = -1          #Plan until convergence to goal
     # tau = 1.2 * resp.tau       #Desired plan should take twice as long as demo
     # dt = 1.0
-    # integrate_iter = 5       #dt is rather large, so this is > 1  
-    # plan = makePlanRequest(x_0, x_dot_0, t_0, goal, goal_thresh, 
+    # integrate_iter = 5       #dt is rather large, so this is > 1
+    # plan = makePlanRequest(x_0, x_dot_0, t_0, goal, goal_thresh,
     #                        seg_length, tau, dt, integrate_iter)
 
     # print plan
--- nodes/squash_stretch.py
+++ nodes/squash_stretch.py
@@ -1,23 +1,31 @@
 #!/usr/bin/env python
 from dmp_interface import *
-import pylab as  plt
-
+import pylab as plt
+import scipy as sp
 
 
 def create_demo_traj_squash(size, dim=1):
     if dim == 1:
-        x = np.linspace(0, np.pi/2.0, size)
+        x = np.linspace(0, np.pi / 2.0, size)
         y = abs(np.cos(x))
     else:
-        x = np.linspace(np.pi/2.0, np.pi, size)
+        x = np.linspace(np.pi / 2.0, np.pi, size)
         y = abs(np.cos(x))
 
     return y.tolist()
 
+
 def create_demo_arc(size):
     x = np.linspace(0, 1, size)
-    return (np.exp(x**2)/np.sqrt(2*np.pi)).tolist()
+    return (np.exp(x ** 2) / np.sqrt(2 * np.pi)).tolist()
 
+
+def create_demo_secondary_action(size):
+    pass
+
+
+def create_demo_acticipation(size):
+    pass
 
 
 def squash_and_stretch():
@@ -26,32 +34,30 @@ def squash_and_stretch():
     # y2 = create_demo_arc(500)
 
     dims = 2
-    dt = 1.0                
-    K = 50                 
-    D = 2.0 * np.sqrt(K)      
+    dt = 1.0
+    K = 50
+    D = 2.0 * np.sqrt(K)
     num_bases = 50
 
     assert(len(y1) == len(y2))
-    traj = [[y1[i],y2[i]] for i in xrange(len(y1))]
+    traj = [[y1[i], y2[i]] for i in xrange(len(y1))]
     resp = makeLFDRequest(dims, traj, dt, K, D, num_bases)
     makeSetActiveRequest(resp.dmp_list)
 
-
     print 'generating plan'
-    #Now, generate a plan
-    x_0 = [y1[0], y2[0]]          #Plan starting at a different point than demo 
-    x_dot_0 = [y1[0], y2[0]]   
-    # x_dot_0 = [y1[0], y1[-1]]   
-    t_0 = 0                
-    goal = [y1[-1], y2[-1]]         #Plan to a different goal than demo
+    # Now, generate a plan
+    x_0 = [y1[0], y2[0]]  # Plan starting at a different point than demo
+    x_dot_0 = [y1[0], y2[0]]
+    # x_dot_0 = [y1[0], y1[-1]]
+    t_0 = 0
+    goal = [y1[-1], y2[-1]]  # Plan to a different goal than demo
     goal_thresh = [0.2, 0.2]
-    seg_length = -1          #Plan until convergence to goal
-    tau = 1.2 * resp.tau       #Desired plan should take twice as long as demo
+    seg_length = -1  # Plan until convergence to goal
+    tau = 1.2 * resp.tau  # Desired plan should take twice as long as demo
     dt = 1.0
-    integrate_iter = 5       #dt is rather large, so this is > 1  
-    plan = makePlanRequest(x_0, x_dot_0, t_0, goal, goal_thresh, 
+    integrate_iter = 5  # dt is rather large, so this is > 1
+    plan = makePlanRequest(x_0, x_dot_0, t_0, goal, goal_thresh,
                            seg_length, tau, dt, integrate_iter)
-
 
     # aggregate reproduced trajectories
     rp1, rp2 = [], []
@@ -66,45 +72,41 @@ def squash_and_stretch():
         rv2.append(p.velocities[1])
 
     plt.figure('squash_stretch dmp learning')
-    plt.plot(y1+y2, label='Demonstrated Policy')
-    plt.plot(rp1+rp2, label='Reproduced Policy (1.2*tau)')
+    plt.plot(y1 + y2, label='Demonstrated Policy')
+    plt.plot(rp1 + rp2, label='Reproduced Policy (1.2*tau)')
     plt.legend(loc='lower right')
 
-
     plt.figure('velocities')
-    plt.plot(rv1+rv2, label='Velocities')
+    plt.plot(rv1 + rv2, label='Velocities')
     plt.show()
-
 
 
 def arc():
     y = create_demo_arc(500)
 
     dims = 1
-    dt = 1.0                
-    K = 50                 
-    D = 2.0 * np.sqrt(K)      
+    dt = 1.0
+    K = 50
+    D = 2.0 * np.sqrt(K)
     num_bases = 50
 
     traj = [[y[i]] for i in xrange(len(y))]
     resp = makeLFDRequest(dims, traj, dt, K, D, num_bases)
     makeSetActiveRequest(resp.dmp_list)
 
-
     print 'generating plan'
-    #Now, generate a plan
-    x_0 = [y[0]]          #Plan starting at a different point than demo 
-    x_dot_0 = [y[0]]   
-    t_0 = 0                
-    goal = [y[-1]]         #Plan to a different goal than demo
+    # Now, generate a plan
+    x_0 = [y[0]]  # Plan starting at a different point than demo
+    x_dot_0 = [y[0]]
+    t_0 = 0
+    goal = [y[-1]]  # Plan to a different goal than demo
     goal_thresh = [0.2]
-    seg_length = -1          #Plan until convergence to goal
-    tau = 0.8 * resp.tau       #Desired plan should take twice as long as demo
+    seg_length = -1  # Plan until convergence to goal
+    tau = 0.8 * resp.tau  # Desired plan should take twice as long as demo
     dt = 1.0
-    integrate_iter = 5       #dt is rather large, so this is > 1  
-    plan = makePlanRequest(x_0, x_dot_0, t_0, goal, goal_thresh, 
+    integrate_iter = 5  # dt is rather large, so this is > 1
+    plan = makePlanRequest(x_0, x_dot_0, t_0, goal, goal_thresh,
                            seg_length, tau, dt, integrate_iter)
-
 
     # aggregate reproduced trajectories
     rp = []
@@ -131,6 +133,3 @@ if __name__ == '__main__':
 
     squash_and_stretch()
     arc()
-
-    
-
